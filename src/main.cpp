45979789503aa37b3add2b6c4b72819870acac56
unknown <Vinicius Ferreira Citadin>
2024-05-22 08:19:39 -0300

    Código atualizado para apenas um joystick

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,6 @@
 
 USBHID HID;
 HIDJoystick Joystick(HID);
-HIDJoystick Joystick_02(HID);
-HIDJoystick Joystick_03(HID);
-HIDJoystick Joystick_04(HID);
 
 void setup() {
   HID.begin(HID_JOYSTICK);
@@ -13,23 +10,11 @@ void setup() {
 }
 
 void loop() {
+  
   Joystick.X(0);
-  delay(500);
+  Joystick.Y(0);
+  Joystick.sliderRight(1023);
   Joystick.X(1023);
-  delay(500);
-
-  Joystick_02.Y(0);
-  delay(500);
-  Joystick_02.Y(1023);
-  delay(500);
-
-  Joystick_03.X(0);
-  delay(500);
-  Joystick_03.Y(1023);
-  delay(500);
-
-  Joystick_04.X(1023);
-  delay(500);
-  Joystick_04.Y(0);
-  delay(500);
+  Joystick.Y(1023);
+  Joystick.sliderRight(0);
 }
